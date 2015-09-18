4cad87bf9e8bbaf3db8257d1cf657bd1afa0d719
Edwin Phillips <edwin.phillips@catalyst-eu.net>
2015-09-18 11:14:15 +0100

    Moodle 2.7 version initial commit

---
+++ lang/en/block_kursus_afvikling.php
@@ -0,0 +1,6 @@
+<?PHP // $Id: block_kursus_afvikling.php 
+$string['blockname'] = 'TUR Kursusafvikling';
+$string['pluginname'] = 'TUR Kursusafvikling';
+$string['showtext'] = 'Vis med tekst';
+$string['hidetext'] = 'Vis uden tekst';
+?>
